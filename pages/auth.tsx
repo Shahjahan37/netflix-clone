b8389997d4dd844e1b160335b41862113e41e6d2
Shahjahan37 <shahjahanalishaikh21@gmail.com>
2025-04-22 23:39:12 +0530

    initial commit

---
+++ pages/auth.tsx
@@ -0,0 +1,16 @@
+const Auth = () => {
+  return (
+    <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-cover">
+      <div className="bg-black/50 w-full h-full">
+        <nav className="px-12 py-5">
+          <img src="/images/logo.svg" alt="Logo" className="h-12" />
+        </nav>
+        <div>
+            
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-2xl text-green-500">Hello</h1>
+    </div>
+  );
+}
